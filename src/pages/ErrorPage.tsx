8bd77eb0beb0e2df3127f1dea276cd8b96157fac
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-07-31 12:07:58 +0800

    nambah error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { MdKeyboardBackspace } from "react-icons/md";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl md:text-5xl font-semibold font-poppins mb-3">
+        Upss...
+      </h1>
+      <p className="font-roboto mb-5">Halaman tidak ditemukan</p>
+      <Link to={"/"}>
+        <div className="px-5 py-3 bg-kong flex items-center justify-center rounded-md font-poppins gap-3 font-medium">
+          <MdKeyboardBackspace />
+          <span>Ke beranda</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Posts from "../pages/Posts/Posts";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
